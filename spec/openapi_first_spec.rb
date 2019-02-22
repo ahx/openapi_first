d55d055e44adff59ea22604df467b32ca7f8c7b2
Jens Zobel <jens.zobel@injixo.com>
2019-02-22 15:25:47 +0100

    Remove unused test

--- spec/openapi_first_spec.rb
+++ spec/openapi_first_spec.rb
@@ -1,9 +1,5 @@
 RSpec.describe OpenapiFirst do
-  it "has a version number" do
+  it 'has a version number' do
     expect(OpenapiFirst::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
