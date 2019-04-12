3326fe0c226e7ca5ca43d69a0ebe8263a01eff1f
Andreas Haller <andreas.haller@posteo.de>
2019-04-13 00:01:42 +0200

    Don't require everything at once

--- lib/openapi_first.rb
+++ lib/openapi_first.rb
@@ -1,7 +1,5 @@
 require 'oas_parser'
 require 'openapi_first/version'
-require 'openapi_first/response_validator'
-require 'openapi_first/test_coverage'
 
 module OpenapiFirst
   def self.load(spec_path)
