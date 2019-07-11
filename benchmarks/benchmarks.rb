5569061106f21847b1df31817a7a0b70800ed5b5
Andreas Haller <andreas.haller@posteo.de>
2019-07-11 12:50:43 +0200

    Add more request examples to benchmarks

--- benchmarks/benchmarks.rb
+++ benchmarks/benchmarks.rb
@@ -7,18 +7,23 @@ ENV['RACK_ENV'] = 'production'
 configs = Dir['./apps/*.ru']
 
 Benchmark.ips do |x|
-  requests = [
-    Rack::MockRequest.env_for('/hello'),
-    Rack::MockRequest.env_for('/unknown'),
-    Rack::MockRequest.env_for('/hello', method: 'POST'),
-    Rack::MockRequest.env_for('/hello/1'),
-    Rack::MockRequest.env_for('/hello/123')
+  examples = [
+    [Rack::MockRequest.env_for('/hello'), 200],
+    [Rack::MockRequest.env_for('/unknown'), 404],
+    [Rack::MockRequest.env_for('/hello', method: 'POST'), 201],
+    [Rack::MockRequest.env_for('/hello/1'), 200],
+    [Rack::MockRequest.env_for('/hello/123'), 200],
+    [Rack::MockRequest.env_for('/hello?filter[id]=1,2'), 200]
   ]
 
   configs.each do |config|
     app = Rack::Builder.parse_file(config).first
     x.report(config) do
-      requests.each { |env| app.call(env) }
+      examples.each do |example|
+        env, expected_status = example
+        response = app.call(env)
+        raise unless response[0] == expected_status
+      end
     end
   end
 
