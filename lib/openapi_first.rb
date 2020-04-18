469892d510d428d7b174cc035e900ed7a1ba7113
Andreas Haller <andreas.haller@posteo.de>
2020-04-18 08:20:31 +0200

    Work around missing require in oas_parser

--- lib/openapi_first.rb
+++ lib/openapi_first.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'yaml'
+require 'oas_parser/path' # TODO: Remove when Nexmo/oas_parser/pull/49 is merged
 require 'oas_parser'
 require 'openapi_first/definition'
 require 'openapi_first/version'
