bb7729de959967a63f7b2bab2da3ebf9068aca2c
Andreas Haller <andreas.haller@posteo.de>
2022-09-01 21:44:54 +0200

    Add POST request body in benchmarks

--- benchmarks/benchmarks.rb
+++ benchmarks/benchmarks.rb
@@ -3,12 +3,13 @@
 require 'benchmark/ips'
 require 'benchmark/memory'
 require 'rack'
+require 'json'
 ENV['RACK_ENV'] = 'production'
 
 examples = [
   [Rack::MockRequest.env_for('/hello'), 200],
   [Rack::MockRequest.env_for('/unknown'), 404],
-  [Rack::MockRequest.env_for('/hello', method: 'POST'), 201],
+  [Rack::MockRequest.env_for('/hello', method: 'POST', input: JSON.dump({say: 'hi!'}), 'CONTENT_TYPE' => 'application/json'), 201],
   [Rack::MockRequest.env_for('/hello/1'), 200],
   [Rack::MockRequest.env_for('/hello/123'), 200],
   [Rack::MockRequest.env_for('/hello?filter[id]=1,2'), 200]
@@ -24,7 +25,7 @@ bench = lambda do |app|
     env, expected_status = example
     100.times { app.call(env) }
     response = app.call(env)
-    raise unless response[0] == expected_status
+    raise "expected status #{expected_status}, but was #{response[0]}" unless response[0] == expected_status
   end
 end
 
