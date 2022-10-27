c00a8063f5d63c7932561630f531ae084e30bd15
Andreas Haller <andreas.haller@posteo.de>
2022-10-27 23:11:53 +0200

    RackResponder inherits Responder

--- lib/openapi_first/rack_responder.rb
+++ lib/openapi_first/rack_responder.rb
@@ -3,33 +3,14 @@
 require 'rack'
 require 'multi_json'
 require_relative 'inbox'
+require_relative 'responder'
 require_relative 'default_operation_resolver'
 
 module OpenapiFirst
-  class RackResponder
-    def initialize(namespace: nil, resolver: nil)
-      @resolver = resolver || DefaultOperationResolver.new(namespace)
-      @namespace = namespace
-    end
-
+  class RackResponder < Responder
     def call(env)
       operation = env[OpenapiFirst::OPERATION]
       find_handler(operation)&.call(env)
-    end
-
-    private
-
-    def find_handler(operation)
-      handler = @resolver.call(operation)
-      raise NotImplementedError, "Could not find handler for #{operation.name}" unless handler
-
-      handler
-    end
-
-    def serialize(result)
-      return result if result.is_a?(String)
-
-      MultiJson.dump(result)
     end
   end
 end
