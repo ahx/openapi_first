ba196af74dde5d722ad33d37774aab95b8043705
Andreas Haller <andreas.haller@posteo.de>
2023-04-05 00:32:00 +0200

    Remove obsolete files and methods
    Format Gemfile

--- lib/openapi_first/operation.rb
+++ lib/openapi_first/operation.rb
@@ -4,10 +4,9 @@ require 'forwardable'
 require 'set'
 require_relative 'schema_validation'
 require_relative 'utils'
-require_relative 'response_object'
 
 module OpenapiFirst
-  class Operation # rubocop:disable Metrics/ClassLength
+  class Operation
     extend Forwardable
     def_delegators :operation_object,
                    :[],
@@ -38,24 +37,6 @@ module OpenapiFirst
 
     def request_body
       operation_object['requestBody']
-    end
-
-    def parameters_schema
-      @parameters_schema ||= begin
-        parameters_json_schema = build_parameters_json_schema
-        parameters_json_schema && SchemaValidation.new(parameters_json_schema)
-      end
-    end
-
-    def query_parameters_schema
-      @query_parameters_schema ||= begin
-        query_parameters_json_schema = build_query_parameters_json_schema
-        query_parameters_json_schema && SchemaValidation.new(query_parameters_json_schema)
-      end
-    end
-
-    def content_types_for(status)
-      response_for(status)['content']&.keys
     end
 
     def response_schema_for(status, content_type)
@@ -137,43 +118,6 @@ module OpenapiFirst
         type = request_content_type.split(';')[0]
         content[type] || content["#{type.split('/')[0]}/*"] || content['*/*']
       end
-    end
-
-    def build_parameters_json_schema
-      parameters = all_parameters
-      return unless parameters&.any?
-
-      parameters.each_with_object(new_node) do |parameter, schema|
-        params = Rack::Utils.parse_nested_query(parameter['name'])
-        generate_schema(schema, params, parameter)
-      end
-    end
-
-    def build_query_parameters_json_schema
-      return unless query_parameters&.any?
-    end
-
-    def generate_schema(schema, params, parameter)
-      required = Set.new(schema['required'])
-      params.each do |key, value|
-        required << key if parameter['required']
-        if value.is_a? Hash
-          property_schema = new_node
-          generate_schema(property_schema, value, parameter)
-          Utils.deep_merge!(schema['properties'], { key => property_schema })
-        else
-          schema['properties'][key] = parameter['schema']
-        end
-      end
-      schema['required'] = required.to_a
-    end
-
-    def new_node
-      {
-        'type' => 'object',
-        'required' => [],
-        'properties' => {}
-      }
     end
   end
 end
