b9cb890c3b3b3035a5c41ff6633c23b7a8f2092b
Andreas Haller <andreas.haller@posteo.de>
2023-04-04 22:50:39 +0200

    Make the example app show what you can do with the router

--- examples/app.rb
+++ examples/app.rb
@@ -3,13 +3,16 @@
 require 'openapi_first'
 require 'rack'
 
+# This example is a bit contrived, but it shows what you could do with the middlewares
+
 App = Rack::Builder.new do
   use OpenapiFirst::RequestValidation, raise_error: true, spec: File.expand_path('./openapi.yaml', __dir__)
   use OpenapiFirst::ResponseValidation
 
-  map '/' do
-    run(lambda do |_env|
-      [200, { 'Content-Type' => 'application/json' }, ['{"hello": "world"}']]
-    end)
-  end
+  handlers = {
+    'things#index' => ->(_env) { [200, { 'Content-Type' => 'application/json' }, ['{"hello": "world"}']] }
+  }
+  not_found = ->(_env) { [404, {}, []] }
+
+  run ->(env) { handlers.fetch(env[OpenapiFirst::OPERATION].operation_id, not_found).call(env) }
 end
