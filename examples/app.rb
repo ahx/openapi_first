0538ad5f8a5acc29c8434d78082a2286a95fd5aa
Andreas Haller <andreas.haller@posteo.de>
2023-04-04 22:32:55 +0200

    Make example app easier to understand

--- examples/app.rb
+++ examples/app.rb
@@ -1,19 +1,15 @@
 # frozen_string_literal: true
 
 require 'openapi_first'
+require 'rack'
 
 App = Rack::Builder.new do
   use OpenapiFirst::RequestValidation, raise_error: true, spec: File.expand_path('./openapi.yaml', __dir__)
   use OpenapiFirst::ResponseValidation
 
-  handlers = {
-    '/' => {
-      'GET' => lambda do |_env|
-        [200, { 'Content-Type' => 'application/json' }, ['{"hello": "world"}']]
-      end
-    }
-  }
-  run(lambda do |env|
-    handlers[env['PATH_INFO']][env['REQUEST_METHOD']].call(env)
-  end)
+  map '/' do
+    run(lambda do |_env|
+      [200, { 'Content-Type' => 'application/json' }, ['{"hello": "world"}']]
+    end)
+  end
 end
