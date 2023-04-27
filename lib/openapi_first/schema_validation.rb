d4002a5325010785062bb5b19e88592ddb7ac9aa
Andreas Haller <andreas.haller@posteo.de>
2023-04-27 18:00:51 +0200

    Slice of some bytest

--- lib/openapi_first/schema_validation.rb
+++ lib/openapi_first/schema_validation.rb
@@ -9,16 +9,13 @@ module OpenapiFirst
     def initialize(schema, write: true)
       @raw_schema = schema
       custom_keywords = {}
-      custom_keywords['writeOnly'] = proc { |data| !data } unless write
-      custom_keywords['readOnly'] = proc { |data| !data } if write
+      custom_keywords['writeOnly'] = method(:fail_if_truthy) unless write
+      custom_keywords['readOnly'] = method(:fail_if_truthy) if write
       @schemer = JSONSchemer.schema(
         schema,
         keywords: custom_keywords,
         insert_property_defaults: true,
-        before_property_validation: proc do |data, property, property_schema, parent|
-          convert_nullable(data, property, property_schema, parent)
-          binary_format(data, property, property_schema, parent)
-        end
+        before_property_validation: method(:before_property_validation)
       )
     end
 
@@ -27,6 +24,15 @@ module OpenapiFirst
     end
 
     private
+
+    def fail_if_truthy(data, _curr_schema, _pointer)
+      !data
+    end
+
+    def before_property_validation(data, property, property_schema, parent)
+      convert_nullable(data, property, property_schema, parent)
+      binary_format(data, property, property_schema, parent)
+    end
 
     def binary_format(data, property, property_schema, _parent)
       return unless property_schema.is_a?(Hash) && property_schema['format'] == 'binary'
