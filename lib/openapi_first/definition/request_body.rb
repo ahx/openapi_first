08331802c8cea97756fe986175c849417f3a51e2
Andreas Haller <andreas.haller@posteo.de>
2023-11-15 15:35:53 +0100

    Memoize schema

--- lib/openapi_first/definition/request_body.rb
+++ lib/openapi_first/definition/request_body.rb
@@ -18,12 +18,18 @@ module OpenapiFirst
     end
 
     def content_for(content_type)
-      content = @request_body_object['content']
-      media_type_object = content&.fetch(content_type) do |_|
+      content&.fetch(content_type) do |_|
         type = content_type.split(';')[0]
         content[type] || content["#{type.split('/')[0]}/*"] || content['*/*']
       end
-      MediaType.new(media_type_object, @operation) if media_type_object
+    end
+
+    private
+
+    def content
+      @content ||= @request_body_object.fetch('content', nil).dup.transform_values! do |media_type_object|
+        MediaType.new(media_type_object, @operation)
+      end
     end
   end
 end
