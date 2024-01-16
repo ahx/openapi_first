ee8fec8a59f74b66ddfb2bd7b975080c4e98a3e1
Andreas Haller <andreas.haller@posteo.de>
2024-01-16 11:20:39 +0100

    Reduce length of failure exception message

--- spec/failure_spec.rb
+++ spec/failure_spec.rb
@@ -16,4 +16,28 @@ RSpec.describe OpenapiFirst::Failure do
       end
     end
   end
+
+  describe '#raise!' do
+    it 'raises an error' do
+      expect do
+        described_class.new(:invalid_body).raise!
+      end.to raise_error(OpenapiFirst::RequestInvalidError)
+    end
+
+    context 'with a lot of errors' do
+      let(:failure) do
+        errors = 100.times.map do |i|
+          instance_double(OpenapiFirst::Schema::ValidationError, error: "something is wrong over there #{i}")
+        end
+        described_class.new(:invalid_body, errors:)
+      end
+
+      it 'raises an error with a reduced message' do
+        expect do
+          failure.raise!
+        end.to raise_error(OpenapiFirst::RequestInvalidError,
+                           'Request body invalid: something is wrong over there 0. something is wrong over there 1. something is wrong over there 2. something is wrong over there 3. ... (100 errors total)') # rubocop:disable Layout/LineLength
+      end
+    end
+  end
 end
