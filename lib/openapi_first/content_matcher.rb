4c19c6e23df0240e7cfdfe9902fcaab79c59f107
Andreas Haller <andreas.haller@posteo.de>
2024-04-28 08:09:15 +0200

    OAS3.0 has only one object per content-type

--- lib/openapi_first/content_matcher.rb
+++ lib/openapi_first/content_matcher.rb
@@ -4,13 +4,15 @@ module OpenapiFirst
   # Finds the request objects that matches the content type.
   class ContentMatcher
     def initialize(requests)
-      @requests = requests.group_by(&:content_type)
+      @requests = requests.each_with_object({}) do |req, result|
+        result[req.content_type] = req
+      end
     end
 
     def call(content_type)
       @requests.fetch(content_type) do
         type = content_type.split(';')[0]
-        @requests[type] || @requests["#{type.split('/')[0]}/*"] || @requests['*/*'] || []
+        @requests[type] || @requests["#{type.split('/')[0]}/*"] || @requests['*/*']
       end
     end
   end
--- spec/content_matcher_spec.rb
+++ spec/content_matcher_spec.rb
@@ -16,31 +16,31 @@ RSpec.describe OpenapiFirst::ContentMatcher do
     subject(:matcher) { described_class.new(requests) }
 
     it 'returns the matching object' do
-      expect(matcher.call('application/json')).to eq([requests[0]])
+      expect(matcher.call('application/json')).to eq(requests[0])
     end
 
     it 'returns empty list if no match' do
-      expect(matcher.call('image/*')).to be_empty
+      expect(matcher.call('image/*')).to be_nil
     end
 
     it 'finds an exact match with parameter' do
       exact = 'application/json; profile=custom'
-      expect(matcher.call(exact).map(&:content_type)).to eq([exact])
+      expect(matcher.call(exact).content_type).to eq(exact)
     end
 
     it 'finds a match while ignoring parameter' do
-      expect(matcher.call('application/xml; Charset=Utf8').map(&:content_type)).to eq(['application/xml'])
+      expect(matcher.call('application/xml; Charset=Utf8').content_type).to eq('application/xml')
     end
 
     it 'finds text/* wildcard matcher' do
-      expect(matcher.call('text/markdown; Charset=Utf8').map(&:content_type)).to eq(['text/*'])
+      expect(matcher.call('text/markdown; Charset=Utf8').content_type).to eq('text/*')
     end
 
     it 'finds */* wildcard matcher' do
       requests = [double(content_type: 'application/json'), double(content_type: '*/*')]
       matcher = described_class.new(requests)
-      expect(matcher.call('some/foobar').map(&:content_type)).to eq(['*/*'])
-      expect(matcher.call('some/foobar; Chartset=utf8').map(&:content_type)).to eq(['*/*'])
+      expect(matcher.call('some/foobar').content_type).to eq('*/*')
+      expect(matcher.call('some/foobar; Chartset=utf8').content_type).to eq('*/*')
     end
   end
 end
