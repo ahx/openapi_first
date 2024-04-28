87357e270a9bfb28076effce9fea5393da95064b
Andreas Haller <andreas.haller@posteo.de>
2024-04-28 13:56:30 +0200

    Return match object in Router

--- lib/openapi_first/router.rb
+++ lib/openapi_first/router.rb
@@ -1,16 +1,23 @@
+# frozen_string_literal: true
+
 require_relative 'path_matcher'
 
 module OpenapiFirst
   # Indexes OpenapiFirst::Request objects by path and request method
   class Router
-    Mismatch = Data.define(:path, :request_method, :error) do
+    Match = Data.define(:path_item, :operation, :params) do
+      def error?
+        false
+      end
+    end
+
+    Mismatch = Data.define(:error) do
       def error?
         true
       end
     end
 
-    PATH_NOT_FOUND_MISMATCH = Mismatch.new(nil, nil, Failure.new(:not_found))
-    NOT_FOUND = [PATH_NOT_FOUND_MISMATCH, nil].freeze
+    PATH_NOT_FOUND = Mismatch.new(Failure.new(:not_found))
 
     # @param requests List of path item definitions
     def initialize(path_items)
@@ -20,12 +27,15 @@ module OpenapiFirst
     # Return all request objects that match the given path and request method
     def match(request_method, path)
       match = @path_matcher.call(path)
-      return NOT_FOUND unless match
+      return PATH_NOT_FOUND unless match
 
-      path_item = match[0]
+      path_item, params = match
       request = path_item.requests[request_method]
-      match[0] = request || Mismatch.new(path_item.path, request_method, Failure.new(:method_not_allowed))
-      match
+      if request
+        Match.new(path_item, request, params)
+      else
+        Mismatch.new(Failure.new(:method_not_allowed))
+      end
     end
   end
 end
--- spec/router_spec.rb
+++ spec/router_spec.rb
@@ -23,32 +23,27 @@ RSpec.describe OpenapiFirst::Router do
       described_class.new(path_items)
     end
 
-    it 'returns the matching object and params' do
-      op, params = router.match('PATCH', '/b')
-      expect(op).to be(requests[1])
-      expect(params).to eq('id' => 'b')
+    it 'returns a match with params' do
+      match = router.match('PATCH', '/b')
+      expect(match.operation).to be(requests[1])
+      expect(match.params).to eq('id' => 'b')
+      expect(match.error?).to eq(false)
     end
 
     it 'returns an incomplete match for unknown path' do
-      match, params = router.match('GET', '/c/d')
-      expect(match.path).to be_nil
-      expect(match.request_method).to be_nil
+      match = router.match('GET', '/c/d')
       expect(match.error?).to eq(true)
       expect(match.error.type).to eq(:not_found)
-      expect(params).to be_nil
     end
 
     it 'returns an incomplete match for unknown request method' do
-      match, params = router.match('DELETE', '/b')
-      expect(match.path).to eq('/{id}')
-      expect(match.request_method).to eq('DELETE')
+      match = router.match('DELETE', '/b')
       expect(match.error?).to eq(true)
       expect(match.error.type).to eq(:method_not_allowed)
-      expect(params).to eq('id' => 'b')
     end
 
     pending 'return what methods are allowed for unknown request method' do
-      match, = router.match('DELETE', '/b')
+      match = router.match('DELETE', '/b')
       expect(match.error.allowed_methods).to eq(%w[GET PATCH])
     end
   end
