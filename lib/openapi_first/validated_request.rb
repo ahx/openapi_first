8f44d0a6642ae373490d720814f425ffcacfbf3c
Andreas Haller <andreas.haller@posteo.de>
2024-05-17 10:14:44 +0200

    Fix comment (Request was removed)

--- lib/openapi_first/validated_request.rb
+++ lib/openapi_first/validated_request.rb
@@ -3,7 +3,7 @@
 require 'forwardable'
 
 module OpenapiFirst
-  # A validated request (see Request). It can be valid or not.
+  # A validated request. It can be valid or not.
   class ValidatedRequest
     extend Forwardable
 
