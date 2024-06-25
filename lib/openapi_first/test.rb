d54b659cb5ae4c970e5db2b94ee9cdecf24f1738
Andreas Haller <andreas.haller@posteo.de>
2024-06-26 00:44:51 +0200

    Improve exception when testing against unregistered OAD

--- lib/openapi_first/test.rb
+++ lib/openapi_first/test.rb
@@ -5,15 +5,23 @@ require_relative 'test/methods'
 module OpenapiFirst
   # Test integration
   module Test
+    class NotRegisteredError < StandardError; end
+
+    DEFINITIONS = {}.freeze
+    def self.definitions = DEFINITIONS
+
     def self.register(path, as: :default)
-      @registry ||= {}
-      @registry[as] = OpenapiFirst.load(path)
+      definitions[as] = OpenapiFirst.load(path)
     end
 
     def self.[](api)
-      @registry[api] || raise(ArgumentError,
-                              "API description #{api} not found to be used via assert_api_conform. " \
-                              'Use OpenapiFirst::Test.register to load an API description first.')
+      definitions.fetch(api) do
+        option = api == :default ? '' : ", as: #{api.inspect}"
+        raise(NotRegisteredError,
+              "API description '#{api.inspect}' not found." \
+              "Please call OpenapiFirst::Test.register('myopenapi.yaml'#{option}) " \
+              'once before calling assert_api_conform.')
+      end
     end
   end
 end
