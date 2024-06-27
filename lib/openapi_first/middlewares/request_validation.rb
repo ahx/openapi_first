f067c36fb62a583c9460105044cee02df8935bde
Andreas Haller <andreas.haller@posteo.de>
2024-06-27 21:21:51 +0200

    Add middleware option to skip rendering a response

--- lib/openapi_first/middlewares/request_validation.rb
+++ lib/openapi_first/middlewares/request_validation.rb
@@ -10,11 +10,13 @@ module OpenapiFirst
       #   :raise_error    A Boolean indicating whether to raise an error if validation fails.
       #                   default: false
       #   :error_response The Class to use for error responses.
+      #                   This can be a Symbol-name of an registered error response (:default, :jsonapi)
+      #                   or it can be set to false to disable returning a response.
       #                   default: OpenapiFirst::Plugins::Default::ErrorResponse (Config.default_options.error_response)
       def initialize(app, options = {})
         @app = app
         @raise = options.fetch(:raise_error, OpenapiFirst.configuration.request_validation_raise_error)
-        @error_response_class = error_response(options[:error_response])
+        @error_response_class = error_response_option(options[:error_response])
 
         spec = options.fetch(:spec)
         raise "You have to pass spec: when initializing #{self.class}" unless spec
@@ -29,17 +31,18 @@ module OpenapiFirst
         validated = @definition.validate_request(Rack::Request.new(env), raise_error: @raise)
         env[REQUEST] = validated
         failure = validated.error
-        return @error_response_class.new(failure:).render if failure
+        return @error_response_class.new(failure:).render if failure && @error_response_class
 
         @app.call(env)
       end
 
       private
 
-      def error_response(mod)
-        return OpenapiFirst.find_error_response(mod) if mod.is_a?(Symbol)
+      def error_response_option(value)
+        return if value == false
+        return OpenapiFirst.find_error_response(value) if value.is_a?(Symbol)
 
-        mod || OpenapiFirst.configuration.request_validation_error_response
+        value || OpenapiFirst.configuration.request_validation_error_response
       end
     end
   end
