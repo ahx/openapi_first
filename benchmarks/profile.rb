9bef3ebb23c1fe8ba56673d5cd62fe1d77beee23
Andreas Haller <ahx@posteo.de>
2025-05-18 16:48:13 +0200

    Don't profile creating the env object

--- benchmarks/profile.rb
+++ benchmarks/profile.rb
@@ -10,7 +10,6 @@ definition.validate_request(Rack::Request.new(env), raise_error: true)
 
 Vernier.trace(out: 'profiles/request_validation_profile.json') do
   1000.times do
-    env = Rack::MockRequest.env_for('/hello?filter[id]=1,2')
     definition.validate_request(Rack::Request.new(env), raise_error: true)
   end
 end
