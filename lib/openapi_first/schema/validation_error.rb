8c66574344da5004bab2fcf32c5260594a3e7381
Andreas Haller <ahx@posteo.de>
2025-10-08 15:13:29 +0200

    Remove deprecated methods which produced a warning since 2.0

--- lib/openapi_first/schema/validation_error.rb
+++ lib/openapi_first/schema/validation_error.rb
@@ -40,24 +40,6 @@ module OpenapiFirst
           "value at #{location} is invalid (#{type.inspect})"
         end
       end
-
-      # @deprecated Please use {#message} instead
-      def error
-        warn 'OpenapiFirst::Schema::ValidationError#error is deprecated. Use #message instead.'
-        message
-      end
-
-      # @deprecated Please use {#data_pointer} instead
-      def instance_location
-        warn 'OpenapiFirst::Schema::ValidationError#instance_location is deprecated. Use #data_pointer instead.'
-        data_pointer
-      end
-
-      # @deprecated Please use {#schema_pointer} instead
-      def schema_location
-        warn 'OpenapiFirst::Schema::ValidationError#schema_location is deprecated. Use #schema_pointer instead.'
-        schema_pointer
-      end
     end
   end
 end
