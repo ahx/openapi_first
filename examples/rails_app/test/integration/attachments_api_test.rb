cba465fe46334c8ef08dff5612962ccf76714fa1
Andreas Haller <ahx@posteo.de>
2025-10-28 19:51:15 +0100

    Point AttachmentsTest to the registered attachments OAD

--- examples/rails_app/test/integration/attachments_api_test.rb
+++ examples/rails_app/test/integration/attachments_api_test.rb
@@ -3,9 +3,12 @@
 require 'test_helper'
 
 class AttachmentsApiTest < ActionDispatch::IntegrationTest
+  # def openapi_first_default_api = :attachments
+  include OpenapiFirst::Test::Methods[TrainTravel::Application, api: :attachments]
+
   test 'GET /attachments/{attachment_id}' do
     get '/attachments/42abc', as: :json
 
-    assert_api_conform(status: 200, api: :attachments)
+    assert_api_conform(status: 200)
   end
 end
