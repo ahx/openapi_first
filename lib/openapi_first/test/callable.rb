23c4fe4e5980f4a3144792db0ae75b0777950155
Andreas Haller <ahx@posteo.de>
2025-11-01 15:23:39 +0100

    Fix description of Callable module

--- lib/openapi_first/test/callable.rb
+++ lib/openapi_first/test/callable.rb
@@ -6,7 +6,7 @@ module OpenapiFirst
     # This is used by Openapi::Test.observe
     module Callable
       # Returns a Module with a `call(env)` method that wraps super inside silent request/response validation
-      # You can use this like `Application.prepend(OpenapiFirst::Test.app_module)` to monitor your app during testing.
+      # You can use `Application.prepend(OpenapiFirst::Test::Callable[oad])` to monitor your app during testing.
       def self.[](definition)
         Module.new.tap do |mod|
           mod.define_method(:call) do |env|
