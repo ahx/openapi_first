18660a0e03951a6510ead2c01e4eb8345e6b38d3
Michael Oberegger <michael.oberegger@affinity.co>
2026-03-31 17:44:52 -0400

    Cache files in FileLoader

--- lib/openapi_first/file_loader.rb
+++ lib/openapi_first/file_loader.rb
@@ -6,17 +6,32 @@ require 'yaml'
 module OpenapiFirst
   # @!visibility private
   module FileLoader
+    @cache = {}
+    @mutex = Mutex.new
+
     module_function
 
     def load(file_path)
-      raise FileNotFoundError, "File not found #{file_path.inspect}" unless File.exist?(file_path)
+      @cache[file_path] || @mutex.synchronize do
+        @cache[file_path] ||= begin
+          raise FileNotFoundError, "File not found #{file_path.inspect}" unless File.exist?(file_path)
 
-      body = File.read(file_path)
-      extname = File.extname(file_path)
-      return ::JSON.parse(body) if extname == '.json'
-      return YAML.unsafe_load(body) if ['.yaml', '.yml'].include?(extname)
+          body = File.read(file_path)
+          extname = File.extname(file_path)
 
-      body
+          if extname == '.json'
+            ::JSON.parse(body)
+          elsif ['.yaml', '.yml'].include?(extname)
+            YAML.unsafe_load(body)
+          else
+            body
+          end
+        end
+      end
+    end
+
+    def clear_cache
+      @mutex.synchronize { @cache.clear }
     end
   end
 end
