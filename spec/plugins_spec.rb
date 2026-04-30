a5001702053f516d15dcbea9039c620f3f997a6b
Andreas Haller <ahx@posteo.de>
2026-04-30 20:58:28 +0200

    Allow enabling plugins via .register

--- spec/plugins_spec.rb
+++ spec/plugins_spec.rb
@@ -7,37 +7,17 @@ RSpec.describe OpenapiFirst::Plugins do
 
   after { OpenapiFirst.instance_variable_set(:@configuration, nil) }
 
-  let(:spec) do
-    {
-      'openapi' => '3.1.0',
-      'info' => { 'title' => 'Test', 'version' => '1' },
-      'paths' => {
-        '/public' => {
-          'get' => {
-            'x-public' => true,
-            'responses' => { '200' => { 'description' => 'ok' } }
-          }
-        },
-        '/private' => {
-          'get' => {
-            'responses' => { '200' => { 'description' => 'ok' } }
-          }
-        }
-      }
-    }
-  end
-
   it 'raises ArgumentError when a plugin does not respond to .configure' do
     stub_const('OpenapiFirst::Plugins::Bad', Module.new)
     expect do
-      OpenapiFirst.parse(spec) { |c| c.plugin :bad }
+      OpenapiFirst.plugin :bad
     end.to raise_error(ArgumentError, /must respond to .configure/)
   end
 
   context 'when loaded globally' do
     it 'applies to all definitions' do
       OpenapiFirst.plugin :x_public
-      definition = OpenapiFirst.parse(spec)
+      definition = OpenapiFirst.load('spec/data/x-public.yaml')
 
       expect(definition.validate_request(build_request('/public'))).to be_valid
       expect(definition.validate_request(build_request('/private'))).to be_invalid
@@ -45,11 +25,19 @@ RSpec.describe OpenapiFirst::Plugins do
   end
 
   context 'when loaded locally' do
-    it 'applies to only this one definition' do
-      with_plugin = OpenapiFirst.parse(spec) { |oad| oad.plugin :x_public }
-      without_plugin = OpenapiFirst.parse(spec)
+    it 'applies to this one definition' do
+      with_public = OpenapiFirst.load('spec/data/x-public.yaml') { |c| c.plugin :x_public }
+      without_public = OpenapiFirst.load('spec/data/x-public.yaml')
 
-      expect(without_plugin.validate_request(build_request('/private'))).to be_valid
+      expect(with_public.validate_request(build_request('/private'))).to be_invalid
+      expect(without_public.validate_request(build_request('/private'))).to be_valid
+    end
+  end
+
+  context 'when loaded via register' do
+    it 'applies to this one definition' do
+      with_plugin = OpenapiFirst.register('spec/data/x-public.yaml') { |api| api.plugin :x_public }
+
       expect(with_plugin.validate_request(build_request('/private'))).to be_invalid
     end
   end
